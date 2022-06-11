78cac731b3c36300b993171e641bf5b4ad421bde
Devin Raleigh <devinkr@gmail.com>
2022-06-11 12:40:41 -0600

    Fix styling

--- src/components/About.js
+++ src/components/About.js
@@ -4,11 +4,10 @@ function About(props) {
 	return (
 		<div className='list'>
 			<h1>React Stocks App</h1>
-			<p>This app was built by Devin, Meesum, and Isaac.</p>
 			<p>
-				It gets up to date stock data from the IEX API. Follow the stock links
-				listed on the dashboard or type a stock symbol into the url in the form
-				of /stocks/symbol
+				This app was built by Devin, Meesum, and Isaac. It gets up to date stock
+				data from the IEX API. Follow the stock links listed on the dashboard or
+				type a stock symbol into the url in the form of /stocks/symbol
 			</p>
 		</div>
 	);
